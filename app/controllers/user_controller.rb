cfafaf57c88b7adafcc52016cb5c34a7cecbead6
John Trenton Cary <johntcary@gmail.com>
2015-10-07 11:39:15 -0500

    change if and else to begin and rescue

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,13 +15,16 @@ class UserController < ApplicationController
   end
 
   def login
-    if params[:username].empty?
+    begin params[:username]
       #To access the value entered into the form use params[:username]
       #So to print out the username that was entered as an alert you 
       #would do flash.now[:alert] = "#{params[:username]} was entered"
-      flash.now[:alert] = "Username Can't be blank!"
-    else
-      
+      if params[:alert].empty?
+        flash.now[:alert] = "Username Can't be blank!"
+      else
+      end
+    rescue
+      flash.now[:alert] = "Username can't be blank"
     end 
   end
 end
