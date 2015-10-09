210121634ebf7ab22043923eca547f70900b3a66
Jacob Donnelly <caynraiser@yahoo.com>
2015-10-08 19:55:41 -0500

    Fixed ticket table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004192403) do
+ActiveRecord::Schema.define(version: 20151009004742) do
 
   create_table "menu_items", force: :cascade do |t|
     t.string   "name"
