1bdef30647b97b3877fd007380409b19766515cc
Casey Carroll <casey.r.carroll@icloud.com>
2015-10-26 12:38:42 -0500

    added updateStatus method to order_items controller. New action will change the numerical status of the order item after the user selects the order item on the kitchen view

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,4 +6,28 @@ class OrderItemsController < ApplicationController
 	
     redirect_to guest_path
   end
+
+  def updateStatus
+  	unClaimed = 0
+  	inProgress = 1
+  	done = 2
+
+
+  	@ticket = Ticket.find(params[:ticket_id])
+  	@orderItem = @ticket.orderItems.find(params[:order_id])
+
+
+  	case @orderItem.istatus  	
+	  	when unClaimed
+	  		@orderItem.istatus = inProgress
+	  	when inProgress
+	  		@orderItem.istatus = done
+	  	when done
+	  		@orderItem.istatus = inProgress
+	  		# check all order items within ticket
+  	end
+
+  	logger.debug
+
+  end
 end
