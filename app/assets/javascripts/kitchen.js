2dd7ae19f078de9b861f4baf741b5229fadfa68e
Casey Carroll <casey.r.carroll@icloud.com>
2015-11-01 16:35:18 -0600

    persistant order item status colors

--- app/assets/javascripts/kitchen.js
+++ app/assets/javascripts/kitchen.js
@@ -9,3 +9,19 @@ function updateStatus(food) {
 		(food).style.backgroundColor = inProgress;
 	}
 }
+
+function loadStatus(status, name) {
+
+	var inProgress = "rgb(255, 255, 163)";
+	var done = "#66FF99";
+
+	alert(status);
+
+	if (status == 0) {
+		
+	} else if (status == 1) {
+		document.getElementById(name).style.backgroundColor = inProgress;
+	} else {
+		document.getElementById(name).style.backgroundColor = done;
+	}
+}
