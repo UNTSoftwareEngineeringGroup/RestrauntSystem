28d6d48266485bcd3a01f778165ef4b251df3ac2
Jacob Donnelly <caynraiser@yahoo.com>
2015-11-01 16:52:04 -0600

    Added top and side nav to order items, added methods to ticket controller

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -3,8 +3,8 @@ class TicketController < ApplicationController
 
 	def addToTicket
   session[:tableID] = "10"
-  ticket = Ticket.where(table: session[:tableID]).first
-  if ticket.nil?
+  ticket = Ticket.where(table: session[:tableID]).last
+  if (ticket.nil?) || (ticket.tstatus == 9)
     ticket = Ticket.create(table: session[:tableID], tax: "8.25", tstatus: 0 )   
     session[:ticket] = check
     puts("**********Ticket created************")
@@ -47,10 +47,34 @@ end
 			check = session[:ticket]
 			check.total = 0
 			OrderItem.where(id: check.id) do |item|
+				check.total = check.total + item.price
 					puts("*****ORDER ITEM FOUND*****")
 			#add all items to total and set total with tax
 			end
 		end
+
+	private
+		def advanceTicket
+			check = session[:ticket]
+			check.tstatus = check.tstatus + 1
+			redirect_to :back
+		end
+
+	private
+		def pay
+			check = session[:ticket]
+			if session[:pay] == "card"#card
+				total = ticket.total + session[:gratuity]
+				
+			else #cash
+				
+
+			end
+			if ticket.total - total == 0
+				check.tstatus = 9
+			end
+		end
+
 
 
 end
