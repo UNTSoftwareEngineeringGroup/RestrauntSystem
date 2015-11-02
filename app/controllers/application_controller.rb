5e6fcf554b59e289eca13752c17bacefbc8ab1a5
alej551 <alej_16064@hotmail.com>
2015-11-01 20:02:13 -0600

    guest button shows user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+ 
+  protected
+
+ 
+  def current_guestaccount
+    # Return nil if the session value isn't set, don't query the DB
+    return nil unless session[:guestaccount_id]
+    # @x ||= y
+    # will make y run only once if it returns a successful value,
+    # essentially caching it for the entire request
+    @current_guestaccount ||= Guestaccount.find(session[:guestaccount_id])
+  end
+  # This makes current_guestaccount, a controller method, accessible in your views.
+  helper_method :current_guestaccount
 end
