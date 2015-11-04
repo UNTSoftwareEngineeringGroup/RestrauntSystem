b1677a8c713f7b6af7846ea03674a6bfdc41d3a6
Casey Carroll <casey.r.carroll@icloud.com>
2015-11-03 22:44:53 -0600

    credit card portion of pay view

---
+++ app/controllers/guest_controller.rb
@@ -0,0 +1,13 @@
+class GuestController < ApplicationController
+	def pay_w_credit
+		
+	end
+	def submit_payment
+		check = Ticket.find_by(table: session[:table_id])
+		check.update(:tstatus => 9)
+		if(check.tstatus == 9) 
+			puts('The ticket has been paid for')
+		end
+		redirect_to guest_path
+	end
+end
