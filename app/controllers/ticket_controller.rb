792351e00709742f93b80e999d0bbc9711570871
Casey Carroll <casey.r.carroll@icloud.com>
2015-11-05 14:50:18 -0600

    add .last per Jake's request

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -39,7 +39,7 @@ class TicketController < ApplicationController
 	end
 
 	def addToTicket
-	  ticket = Ticket.find_by(table: session[:table_id])
+	  ticket = Ticket.find_by(table: session[:table_id]).last
 	  if (ticket.nil?) || (ticket.tstatus == 9)
 	    ticket = Ticket.create(table: session[:table_id], tax: 0, tstatus: 0 )   
 	    session[:ticket] = ticket
