3fefa827e4259b45d0c2d38498517293c9a204b5
alej551 <alej_16064@hotmail.com>
2015-11-08 13:03:33 -0600

    fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,19 @@ class SessionsController < ApplicationController
 def new
 end
 
-
+#Logs the user in and redirect to the guest home page
 def create
   @guestaccount = Guestaccount.find_by_email(params[:session][:email])
   if @guestaccount && @guestaccount.authenticate(params[:session][:password])
     session[:guestaccount_id] = @guestaccount.id
-    
+    redirect_to '/guest?' 
   else
-   flash.now[:danger] = "Invalid email/password combination"# Not quite right!
+   flash.now[:danger] = "Invalid email/password combination"# alert if wrong password/email combo entered
       render 'new'
   end 
 end
 
+#log out out current user
 def destroy 
 	  session[:guestaccount_id] = nil 
 	  
