1bca9004dcab2bda87753566ef9dbfb320c82dc5
Casey Carroll <casey.r.carroll@icloud.com>
2015-11-13 08:15:20 -0600

    basic email template

--- app/mailers/credit_mailer.rb
+++ app/mailers/credit_mailer.rb
@@ -1,2 +1,8 @@
 class CreditMailer < ApplicationMailer
+	default from: 'caseycarroll2@my.unt.edu'
+
+	def creditpay_email(user)
+		@user = user
+		mail(to: @user.email, subject: 'Your Payment at BryceBurger')
+	end
 end
